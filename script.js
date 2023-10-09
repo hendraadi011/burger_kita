dd7c27c3bf4a12df1849e256f61248a0641d80ac
hendraadi011 <hendraadisaputra011@gmail.com>
2023-10-09 14:54:02 +0700

    branch pertama

---
+++ script.js
@@ -0,0 +1,11 @@
+//toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+
+//ketika hamburger menu diklik
+
+document.querySelector('#hamburger').onclick = () => {
+    // navbarNav.classList.toggle('active');
+    console.log('halo')
+}
